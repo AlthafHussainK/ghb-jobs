5e1325f3f761484242970de467bf7f1707b40c65
AlthafHussainK <althafakm@gmail.com>
2021-06-24 22:57:45 +0530

    useFetchHook sample

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import useFetchJobs from "./useFetchHooks";
+import { Container } from 'react-bootstrap'
 
 function App() {
+  const { jobs, loading, error } = useFetchJobs()
+
   return (
-    <h1>Hello React</h1>
+    <Container>
+      {loading && <h1>Loading...</h1>}
+      {error && <h1>Error. Try Refreshing..</h1>}
+      <h1>{jobs.length}</h1>
+    </Container>
   );
 }
 
